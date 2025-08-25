07d6c141c58be4dca443bb10bc49e5e8bbd5fded
Natalia <natala.serdakova562@gmail.com>
2025-08-25 20:37:01 +0300

    Basic concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const sqrtD = Math.sqrt(discriminant);
+
+		const root1 = (-b + sqrtD) / (2 * a);
+		const root2 = (-b - sqrtD) / (2 * a);
+		return [root1, root2];
+	}
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	const monthlyPercent = (percent / 100) / 12;
+	const loanAmount = amount - contribution;
+
+	if (loanAmount <= 0) {
+		return 0;
+	}
+
+	const numerator = monthlyPercent * Math.pow(1 + monthlyPercent, countMonths);
+	const denominator = Math.pow(1 + monthlyPercent, countMonths) - 1;
+	const annuityPayment = loanAmount * (numerator / denominator);
+
+	const totalPayment = annuityPayment * countMonths;
+
+	return Number(totalPayment.toFixed(2));
 }
